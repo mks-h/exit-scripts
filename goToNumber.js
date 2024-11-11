431f42c3474dea3220be0b4afb89c25693170b4b
Maksym Hazevych <mhazevych@proton.me>
2024-11-11 21:17:42 +0200

    Initial commit

---
+++ goToNumber.js
@@ -0,0 +1,44 @@
+javascript: (() => {
+	const openTrackCloseBtn = document.querySelectorAll(
+		".ngneat-close-dialog.ng-star-inserted",
+	)[0];
+	if (openTrackCloseBtn) {
+		openTrackCloseBtn.dispatchEvent(new MouseEvent("click"));
+	}
+
+	const [prevPageBtn, nextPageBth] = ["Ліва стрілка", "Права стрілка"].map(
+		(v) => document.querySelectorAll(`[alt="${v}"]`)[0].parentElement,
+	);
+
+	const pressRepeatadly = (btn, amount) => {
+		const event = new MouseEvent("click");
+		for (let i = 0; i < amount; i++) {
+			btn.dispatchEvent(event);
+		}
+	};
+
+	const switchPage = (targetPage) => {
+		const [currentPage, totalPages] = document
+			.querySelectorAll(".paginator")[0]
+			.innerText.split(" / ");
+		const pagesMargin = targetPage - currentPage;
+		pressRepeatadly(
+			pagesMargin < 0 ? prevPageBtn : nextPageBth,
+			Math.abs(pagesMargin),
+		);
+	};
+
+	const input = prompt("Перейти на учасника №:");
+	if (!input) return;
+	const targetNumber = Number(input);
+	if (!targetNumber) return alert(`Введений текст не підходить: ${input}`);
+	const targetPage = Math.ceil(targetNumber / 15);
+	switchPage(targetPage);
+
+	const getTracks = () =>
+		document.querySelectorAll(".audio-container-row.ng-star-inserted");
+	const targetRelNum = targetNumber % 15;
+	const trackElem = getTracks()[targetRelNum === 0 ? 14 : targetRelNum - 1];
+	const mark = trackElem.querySelectorAll(".mark")[0];
+	mark.dispatchEvent(new MouseEvent("click"));
+})();
